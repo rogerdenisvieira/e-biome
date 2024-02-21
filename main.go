a44b44728e945a6fe5458a82a2db87c6c6b89827
Roger Vieira <roger.vieira@sap.com>
2024-02-21 12:27:25 -0300

    project setup:

---
+++ main.go
@@ -0,0 +1,29 @@
+package main
+
+import (
+	"github.com/hajimehoshi/ebiten"
+)
+
+type Game struct{}
+
+func (g *Game) Update(screen *ebiten.Image) error {
+	return nil
+}
+
+func (g *Game) Draw(screen *ebiten.Image) {
+	// render the screen
+}
+
+func (g *Game) Layout(outsideWidth, outsideHeight int) (screenWidth, screenHeight int) {
+	return 320, 240
+}
+
+func main() {
+	ebiten.SetWindowSize(640, 480)
+	ebiten.SetWindowTitle("E-biome")
+	game := &Game{}
+
+	if err := ebiten.RunGame(game); err != nil {
+		panic(err)
+	}
+}
---
+++ subject/subject.go
@@ -0,0 +1,13 @@
+package subject
+
+type Subject struct {
+	Id      string
+	Stamina int
+	Health  int
+	IsAlive bool
+	Speed   int
+}
+
+func init() {
+	
+}
